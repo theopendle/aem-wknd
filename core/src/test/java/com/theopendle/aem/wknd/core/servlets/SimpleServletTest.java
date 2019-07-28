57864bdb7541969e6ed4cbd450c6d83c04ab6dd6
theopendle <theopendle@gmail.com>
2019-07-28 18:26:01 +0200

    Add all content

---
+++ core/src/test/java/com/theopendle/aem/wknd/core/servlets/SimpleServletTest.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2018 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.theopendle.aem.wknd.core.servlets;
+
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(AemContextExtension.class)
+class SimpleServletTest {
+
+    private final SimpleServlet fixture = new SimpleServlet();
+
+    @Test
+    void doGet(final AemContext context) throws ServletException, IOException {
+        context.build().resource("/content/test", "jcr:title", "resource title").commit();
+        context.currentResource("/content/test");
+
+        final MockSlingHttpServletRequest request = context.request();
+        final MockSlingHttpServletResponse response = context.response();
+
+        fixture.doGet(request, response);
+
+        assertEquals("Title = resource title", response.getOutputAsString());
+    }
+}
